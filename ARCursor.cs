1a943fd9b62c7fe7cdfd6d52ba32360b0b829f5c
Jeremy Lau <52184467+JeremyLau01@users.noreply.github.com>
2022-05-31 12:54:16 -0400

    Add files via upload

---
+++ ARCursor.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCursor : MonoBehaviour
+{
+    public GameObject cursorChildObject;
+    public GameObject objectToPlace;
+    public ARRaycastManager raycastManager;
+
+    public bool useCursor = true;
+
+    GameObject hoop = objectToPlace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorChildObject.SetActive(useCursor);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useCursor)
+        {
+            UpdateCursor();
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (useCursor)
+            {
+                Destroy(hoop);
+                GameObject.Instantiate(hoop, transform.position, transform.rotation);
+            }
+            else
+            {
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+                raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+                if (hits.Count > 0)
+                {
+                    Destroy(hoop);
+                    GameObject.Instantiate(hoop, hits[0].pose.position, hits[0].pose.rotation);
+                }
+            }
+        }
+    }
+
+    void UpdateCursor()
+    {
+        Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+        }
+    }
+}

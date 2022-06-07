a7dc9010e15f142b8bad14b357ae892986007db4
Jeremy Lau <52184467+JeremyLau01@users.noreply.github.com>
2022-06-07 13:53:37 -0400

    Add files via upload

--- RecordToggle.cs
+++ RecordToggle.cs
@@ -5,44 +5,55 @@ using UnityEngine.UI;
 
 public class RecordToggle : MonoBehaviour
 {
+    // For detecting click
     public Button StartRecord;
     public Button StopRecord;
 
+    public Button AssetStartRec;
+    public Button AssetStopRec;
+    public Button AssetViewRec; // no custom button yet :)
+    //public Button AssetSaveRec; - when get this working in the first place :)
+
+    // For SetActive (can only do on game objects)
     public GameObject StartR;
     public GameObject StopR;
 
-    bool StartRec = true;
-    bool StopRec = false;
+    // Keeps track of current states of buttons
+    bool StartRecActive = true;
+    bool StopRecActive = false;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        StartRecord.onClick.AddListener(ClickOn);
-        StopRecord.onClick.AddListener(ClickOff);
+        StartRecord.onClick.AddListener(ClickButton);
+        StopRecord.onClick.AddListener(ClickButton);
         UpdateButtons();
     }
 
-    void ClickOn()
+    void ClickButton()
     {
-        StartRec = !StartRec;
-        StopRec = !StopRec;
+        StartRecActive = !StartRecActive;
+        StopRecActive = !StopRecActive;
         UpdateButtons();
+        // Click asset buttons according to the current boolean values
+        if (StopRecActive)
+        {
+            AssetStartRec.onClick.Invoke();
+        }
+        else
+        {
+            AssetStopRec.onClick.Invoke();
+        }
 
-    }
 
-    void ClickOff()
-    {
-        ClickOn();
     }
 
     void UpdateButtons()
     {
-        Debug.Log(StartRec + " Start");
-        Debug.Log(StopRec + " Stop");
-        StartR.SetActive(StartRec);
-        StopR.SetActive(StopRec);
+        StartR.SetActive(StartRecActive);
+        StopR.SetActive(StopRecActive);
     }
 
     // Update is called once per frame
